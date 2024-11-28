7a2d922439e2aca86d3c968b892e8bb534a06ba9
Sankhat Shivbhadrasinh <112846791+Shivrajbanna609@users.noreply.github.com>
2024-11-28 18:58:03 +0530

    chatbos done

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -10,6 +10,34 @@ const ChatBox = () => {
         <p>Raju Rastogi <img className='dot' src={assets.green_dot} alt="" /></p>
         <img src={assets.help_icon} className='help' alt="" />
       </div>
+
+
+    <div className="chat-msg">
+        <div className="s-msg">
+            <p className="msg">Lorem ipsum dolor sit amet consectetur adipisicing </p>
+            <div>
+                <img src={assets.profile_img} alt="" />
+                <p>2:30</p>
+            </div>
+        </div>
+        <div className="s-msg">
+           <img className='msg-img' src={assets.pic1} alt="" />
+            <div>
+                <img src={assets.profile_img} alt="" />
+                <p>2:30</p>
+            </div>
+        </div>
+        <div className="r-msg">
+            <p className="msg">Lorem ipsum dolor sit amet consectetur adipisicing </p>
+            <div>
+                <img src={assets.profile_img} alt="" />
+                <p>2:30</p>
+            </div>
+        </div>
+    </div>
+
+
+
       <div className="chat-input">
         <input type="text" placeholder='Send a message' />
         <input type="file" id='image' accept='image/png, image/jpeg' hidden />
