9c9fb3ac0555f8e0a5306caa226e72a8b46f7f36
Sankhat Shivbhadrasinh <112846791+Shivrajbanna609@users.noreply.github.com>
2024-11-30 17:35:58 +0530

    search and save user done

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -42,22 +42,23 @@ const AppContextProvider = (props)=>{
     useEffect(()=>{
         if (userData) {
             const chatRef = doc(db, 'chats', userData.id);
-            const unSub = onSnapshot(chatRef, async(res) => {
-                const chatItems = res.data().chatData;
+            const unSub = onSnapshot(chatRef,async (res) => {
+                const chatItems = res.data().chatsData;
                 const tempData = [];
-                for(const item of chatItems) {
-                    const userRef = doc(db, 'users', item.rId);
+                for(const item of chatItems){
+                    const userRef = doc(db,'users', item.rId);
                     const userSnap = await getDoc(userRef);
                     const userData = userSnap.data();
                     tempData.push({...item,userData})
                 }
-                setChatData(tempData.sort((a,b)=>b.updatedAt = a.updatedAt))
+                setChatData(tempData.sort((a,b)=>b.updatedAt - a.updatedAt));
+               
             })
             return ()=>{
                 unSub();
             }
         }
-    })
+    },[userData])
 
     const value = {
         userData, setUserData, chatData, setChatData, loadUserData
